df64c14f7a1860656bb9007b91c1b7360be1f219
AmirHossein Nazari <hnirsnazari@yahoo.com>
2019-07-25 08:36:13 +0430

    init the bit-calculations branch

---
+++ test.py
@@ -0,0 +1,15 @@
+# Kiana jan ino lotfan avaz nakon, mamnonam
+from hextobin import convertErrorPatternIntoBitGeneration as IntoBitGeneration , convertErrorPatternIntoBitSymbol as IntoBitSymbol
+
+
+if __name__ == '__main__':
+    # test the evalution 
+    sentPacket = ['53', '65', '6E', '64', '69', '6E', '67', '20', '4D', '65', '73', '73', '61', '67',
+              '65', '20', '49', '73', '20', '57', '6F', '72', '6B', '69', '6E', '67', '20', '46', '69', '6E', '65']
+
+
+    # just convert a generation to bit representation
+    # bitRep = 
+
+
+    
